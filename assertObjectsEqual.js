1e8df3a1759da958c420cf0b1f3f2aa5cf15531c
curriecode <melaniecurrie01@gmail.com>
2019-09-19 23:14:31 -0700

    "assertEqaualObject function is implimented providing correct output"

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual !== expected) {
-    console.log(`❌❌❌ Assertion Failed: is not a match`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: is a match`);
-  }
-};
-
 const eqObjects = function (object1, object2) {
   let obj1Key = Object.keys(object1).sort();
   let obj2Key = Object.keys(object2).sort();
@@ -38,18 +30,16 @@ const eqObjects = function (object1, object2) {
   return true;
 };
 
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require('util').inspect;
 
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} matches ${inspect(expected)}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} does not match ${inspect(expected)}`);
+  }
+};
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
+assertObjectsEqual({ a: 1, b: 2 }, { b: 2, a: 1 });
+assertObjectsEqual({ a: 2, b: ["1", 2] }, { a: 1, b: 2, });
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false); // => false
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false); // => false
