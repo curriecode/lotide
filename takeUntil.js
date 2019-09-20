33664c17f7bd40a2248379f00e6782c1cbda9310
curriecode <melaniecurrie01@gmail.com>
2019-09-19 18:30:22 -0700

    "tested with assert array"

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,20 @@
+const eqArrays = function (arr1, arr2) {
+  for (let i = 0; i <= arr1.length - 1; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: is a match`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: is no match`);
+  }
+};
+
+
 const takeUntil = (arr, callback) => {
   let newArray = [];
   for (let item of arr) {
@@ -13,10 +30,10 @@ const takeUntil = (arr, callback) => {
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
 
 console.log('---');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
