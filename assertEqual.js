34482a4c1547bc154fc83ebcc93ba214c3bc79e8
curriecode <melaniecurrie01@gmail.com>
2019-09-23 12:30:33 -0700

    "refactored assert equal and head to require/ export"

--- assertEqual.js
+++ assertEqual.js
@@ -1,11 +1,10 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual !== expected) {
-    console.log(`❌❌❌ Assertion Failed: ${actual  !==  expected}`);
+    console.log(`❌❌❌ Assertion Failed: ${actual !== expected}`);
   } else {
     console.log(`✅✅✅ Assertion Passed: ${actual === expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+module.exports = assertEqual;
